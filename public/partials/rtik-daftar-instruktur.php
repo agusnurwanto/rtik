12f87711d9bcd8315cdc072e70ea8ada9e26951a
agusnurwanto <agusnurwantomuslim@gmail.com>
2022-07-20 10:04:39 +0700

    perbaikan daftar instruktur

--- public/partials/rtik-daftar-instruktur.php
+++ public/partials/rtik-daftar-instruktur.php
@@ -22,19 +22,21 @@ foreach($query->posts as $post){
     $no++;
     $link = get_permalink($post->ID);
     $daftar_instruktur .= '
-        <div class="card" style="width: 18rem;" id_pelatihan="'.$post->ID.'">
-            <img class="card-img-top" src="'.get_post_meta($post->ID, '_meta_foto', true).'" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">'.get_post_meta($post->ID, '_meta_nama', true).'</h5>
-                <p class="card-text">'.get_post_meta($post->ID, '_meta_deskripsi', true).'</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">'.get_post_meta($post->ID, '_meta_jabatan', true).'</li>
-                <li class="list-group-item">'.get_post_meta($post->ID, '_meta_sosmed', true).'</li>
-                <li class="list-group-item">'.get_post_meta($post->ID, '_meta_alamat', true).'</li>
-            </ul>
-            <div class="card-body">
-                <a href="'.get_permalink($post->ID).'" class="card-link">Detail</a>
+        <div class="col-md-3 mx-auto">
+            <div class="card" id_pelatihan="'.$post->ID.'" style="margin-bottom: 10px;">
+                <img class="card-img-top" src="'.get_post_meta($post->ID, '_meta_foto', true).'" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">'.get_post_meta($post->ID, '_meta_nama', true).'</h5>
+                    <p class="card-text">'.get_post_meta($post->ID, '_meta_deskripsi', true).'</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">'.get_post_meta($post->ID, '_meta_jabatan', true).'</li>
+                    <li class="list-group-item">'.get_post_meta($post->ID, '_meta_sosmed', true).'</li>
+                    <li class="list-group-item">'.get_post_meta($post->ID, '_meta_alamat', true).'</li>
+                </ul>
+                <div class="card-body">
+                    <a href="'.get_permalink($post->ID).'" class="card-link">Detail</a>
+                </div>
             </div>
         </div>
         ';
@@ -50,10 +52,15 @@ foreach($query->posts as $post){
     td {
         word-break: break-word;
     }
+    .list-group {
+        margin: 0;
+    }
 </style>
 <div class="cetak">
     <div style="padding: 10px;">
         <h2 class="text-center">Daftar Instruktur</h2>
+        <div class="row">
         <?php echo $daftar_instruktur; ?>
+        </div>
     </div>
 </div>
